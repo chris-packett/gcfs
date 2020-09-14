ae2c002005d5ceb636e2ec1a01263a38c2b63ade
Chris <chrispackett1998@gmail.com>
2020-09-14 00:28:25 -0400

    adds zoom to google cloud storage google cloud function

---
+++ ZoomGoogleStorage/index.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+const { snakeCase } = require('snake-case');
+const { Storage } = require('@google-cloud/storage');
+
+const storageClient = new Storage();
+
+const bucketName = 'mr-automation-zoom-recordings';
+
+const zoomRecordingsBucket = storageClient.bucket(bucketName);
+
+async function uploadFile (fileName, downloadUrl, accessToken) {
+    const file = zoomRecordingsBucket.file(fileName);
+    const writeStream = file.createWriteStream();
+
+    const res = await fetch(`${downloadUrl}?access_token=${accessToken}`);
+
+    res.body.pipe(writeStream);
+}
+
+
+exports.uploadZoomRecordingToGoogleStorage = (req, res) => {
+    const { fileName, fileExt, downloadUrl, accessToken } = req.body;
+
+    const formattedFileName = `${snakeCase(fileName)}.${fileExt}`;
+
+    uploadFile(formattedFileName, downloadUrl, accessToken);
+
+    res.status(200).send(`Uploaded ${fileName} to Google Storage Bucket: ${bucketName}`)
+}
